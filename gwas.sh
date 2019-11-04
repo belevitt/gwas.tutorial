94c4785808ef4391d25188bf94de1038e80bf650
Brandt Levitt <belevitt@longleaf-addhealth.its.unc.edu>
2019-11-04 12:59:47 -0500

    sixth commit

--- gwas.sh
+++ gwas.sh
@@ -8,25 +8,25 @@ cmd="sbatch --time=3-0:00:00 --mem=80g"
 #Only modify stuff up here, please
 
 #Enter the path and name for your phenotype file
-file_pheno="~/phenofile.050619"
+file_pheno="/ifs/sec/cpc/addhealth/users/belevitt/datamanagement/toydataset/testrun/phenofile.050619"
 
 #Enter the name of your outcome variable
 outcome_variable="outcome1"
 
 #Enter the name of your covariates
 covariate_variable="pop"
-principle_components="pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+pc11+pc12+pc13+pc14+pc15+pc16+pc17+pc18+pc19+pc20"
+principle_components="PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10+PC11+PC12+PC13+PC14+PC15+PC16+PC17+PC18+PC19+PC20"
 
 #Enter where you want your results to be saved
-output_wd="~"
+output_wd="/ifs/sec/cpc/addhealth/users/belevitt/datamanagement/toydataset/testrun"
 
 #############################################################################################################
 #############################################################################################################
 #############################################################################################################
 
-software="~/SUGEN"
+software="/ifs/sec/cpc/addhealth/apps/bin/SUGEN"
 chrs=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22")
-dir_vcf="~"
+dir_vcf="/ifs/sec/cpc/addhealth/users/belevitt/datamanagement/toydataset/testrun"
 id_col="sample"
 fam_col="gfam"
 quant_formulae=("${outcome_variable}=${principle_components}+${covariate_variable}")
@@ -53,9 +53,9 @@ do
 	
 	for chr in ${chrs[@]}
 	do	
-		file_vcf=${dir_vcf}/chr${chr}RM.dose.vcf.gz
-		file_lsflog=${trait}.chr${chr}RM.lsflog
-		echo "${cmd} --mem=4g -t 4-00:00:00 -n 2 -o ${file_lsflog} --wrap=\"${software} --pheno ${file_pheno} --id-col ${id_col} --vcf ${file_vcf} --formula ${formula} --unweighted --hetero-variance super_pop --out-prefix ${trait}.chr${chr}RM --dosage\"" >> ${file_submit}
+		file_vcf=${dir_vcf}/chr${chr}.vcf.gz
+		file_lsflog=${trait}.chr${chr}.lsflog
+		echo "${cmd} --mem=4g -t 4-00:00:00 -n 2 -o ${file_lsflog} --wrap=\"${software} --pheno ${file_pheno} --id-col ${id_col} --vcf ${file_vcf} --formula ${formula} --unweighted --hetero-variance super_pop --out-prefix ${trait}.chr${chr}\"" >> ${file_submit}
 	done
 done
 
@@ -72,9 +72,9 @@ do
 	
 	for chr in ${chrs[@]}
 	do
-		file_vcf=${dir_vcf}.chr${chr}RM.dose.vcf.gz
-		file_lsflog=${trait}.chr${chr}RM.lsflog
-		echo "${cmd} --mem=4g -t 4-00:00:00 -n 2 -o ${file_lsflog} --wrap=\"${software} --pheno ${file_pheno} --id-col ${id_col} --vcf ${file_vcf} --formula ${formula} --unweighted --hetero-variance super_pop --out-prefix ${trait}.chr${chr}RM --dosage\"" >> ${file_submit}
+		file_vcf=${dir_vcf}.chr${chr}.vcf.gz
+		file_lsflog=${trait}.chr${chr}.lsflog
+		echo "${cmd} --mem=4g -t 4-00:00:00 -n 2 -o ${file_lsflog} --wrap=\"${software} --pheno ${file_pheno} --id-col ${id_col} --vcf ${file_vcf} --formula ${formula} --unweighted --hetero-variance super_pop --out-prefix ${trait}.chr${chr}\"" >> ${file_submit}
 	done
 done
 
