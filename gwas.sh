3f01005e21b02390a467acf574499ede0094d860
Brandt Levitt <belevitt@longleaf-addhealth.its.unc.edu>
2019-11-04 11:20:12 -0500

    fourth commit

--- gwas.sh
+++ gwas.sh
@@ -8,26 +8,26 @@ cmd="sbatch --time=3-0:00:00 --mem=80g"
 #Only modify stuff up here, please
 
 #Enter the path and name for your phenotype file
-file_pheno="/ifs/sec/cpc/addhealth/users/adamg93/pheno"
+file_pheno="~/phenofile.050619"
 
 #Enter the name of your outcome variable
-outcome_variable="bmi"
+outcome_variable="outcome1"
 
 #Enter the name of your covariates
-covariate_variable="race+bio.sex4+age2+weights"
+covariate_variable="pop"
 principle_components="pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+pc11+pc12+pc13+pc14+pc15+pc16+pc17+pc18+pc19+pc20"
 
 #Enter where you want your results to be saved
-output_wd="/ifs/sec/cpc/addhealth/users/adamg93/"
+output_wd="~"
 
 #############################################################################################################
 #############################################################################################################
 #############################################################################################################
 
-software="/ifs/sec/cpc/addhealth/apps/bin/SUGEN"
+software="~/SUGEN"
 chrs=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22")
-dir_vcf="/ifs/sec/cpc/addhealth/gwas/final/1KG/rmfiles"
-id_col="gpairid_aid"
+dir_vcf="~"
+id_col="sample"
 fam_col="gfam"
 quant_formulae=("${outcome_variable}=${principle_components}+${covariate_variable}")
 
@@ -55,7 +55,7 @@ do
 	do	
 		file_vcf=${dir_vcf}/chr${chr}RM.dose.vcf.gz
 		file_lsflog=${trait}.chr${chr}RM.lsflog
-		echo "${cmd} --mem=4g -t 4-00:00:00 -n 2 -o ${file_lsflog} --wrap=\"${software} --pheno ${file_pheno} --id-col ${id_col} --family-col ${fam_col} --vcf ${file_vcf} --formula ${formula} --unweighted --hetero-variance race --out-prefix ${trait}.chr${chr}RM --dosage\"" >> ${file_submit}
+		echo "${cmd} --mem=4g -t 4-00:00:00 -n 2 -o ${file_lsflog} --wrap=\"${software} --pheno ${file_pheno} --id-col ${id_col} --vcf ${file_vcf} --formula ${formula} --unweighted --hetero-variance super_pop --out-prefix ${trait}.chr${chr}RM --dosage\"" >> ${file_submit}
 	done
 done
 
@@ -74,7 +74,7 @@ do
 	do
 		file_vcf=${dir_vcf}.chr${chr}RM.dose.vcf.gz
 		file_lsflog=${trait}.chr${chr}RM.lsflog
-		echo "${cmd} --mem=4g -t 4-00:00:00 -n 2 -o ${file_lsflog} --wrap=\"${software} --pheno ${file_pheno} --id-col ${id_col} --family-col ${fam_col} --vcf ${file_vcf} --formula ${formula} --unweighted --hetero-variance race --out-prefix ${trait}.chr${chr}RM --dosage\"" >> ${file_submit}
+		echo "${cmd} --mem=4g -t 4-00:00:00 -n 2 -o ${file_lsflog} --wrap=\"${software} --pheno ${file_pheno} --id-col ${id_col} --vcf ${file_vcf} --formula ${formula} --unweighted --hetero-variance super_pop --out-prefix ${trait}.chr${chr}RM --dosage\"" >> ${file_submit}
 	done
 done
 
